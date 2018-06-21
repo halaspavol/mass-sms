d3f25c4c219c8aa9280d8c7204b1dcb5ece88e3a
Pavol Halás <pavol.halas1@outlook.com>
2018-06-21 13:56:20 +0200

    optimised manifest, index and added new favicon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Test from './components/Test'
-import './App.css';
+import './App.sass';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Test />
+                
+        
       </div>
     );
   }
