8ccc3fba42c6e905ef0785979e21c24defd6ae0e
Pavol Halás <pavol.halas1@outlook.com>
2018-06-21 16:56:13 +0200

    added layout and aux high order components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import './App.sass';
+import React, { Component } from 'react'
+import './App.sass'
+import Layout from './hoc/Layout/Layout'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-                
-        
+        <Layout />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Aux from '../Auxiliary/Auxiliary'
+import './Layout.sass'
+
+const layout = (props) => (
+   <Aux>
+   
+   </Aux>
+)
+
+export default layout
