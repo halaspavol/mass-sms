bf8b8bb8a4bcfc3f9271a39776b9c23dbe4372aa
Pavol Halás <pavol.halas1@outlook.com>
2018-06-21 19:47:49 +0200

    added axios package, configured axios globaly, tested POST request

--- src/containers/SmsContainer.js
+++ src/containers/SmsContainer.js
@@ -1,9 +1,33 @@
 import React, {Component} from 'react'
- 
+import axios from 'axios'
+
+import Aux from './../hoc/Auxiliary/Auxiliary'
+
 class SmsContainer extends Component {
+   
+   
    render() {
+      
+      // axios.get('v4/device/94327')
+      //    .then(res => console.log(res))
+      //    .catch(err => console.log(err))
+
+      // Posts request to send a text message
+      axios.post('v4/message/send',  [{
+         'phone_number': '0908600565',
+         'message': 'Test',
+         'device_id': 123
+      }])
+         .then(response => console.log(response))
+         .catch(error => console.log(error))
+
+
       return(
-         
+         <Aux>
+            <div>
+               Sem bude form
+            </div>
+         </Aux>
       )
    }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.sass';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.sass'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import axios from 'axios'
+const API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhZG1pbiIsImlhdCI6MTUyOTU5NDEzMSwiZXhwIjo0MTAyNDQ0ODAwLCJ1aWQiOjU1NDEzLCJyb2xlcyI6WyJST0xFX1VTRVIiXX0.NIx2VK68STZU-kJ4q_YphKX2WuQmbnBCE0z3gxLJLhA'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+axios.defaults.baseURL = 'https://smsgateway.me/api'
+axios.defaults.headers.common['Authorization'] = API_KEY
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
