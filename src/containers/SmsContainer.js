ee448606f111617009d8b7f86f885069f8a7dc13
Pavol Halás <pavol.halas1@outlook.com>
2018-06-21 17:12:39 +0200

    added SmsContainer

---
+++ src/containers/SmsContainer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+ 
+class SmsContainer extends Component {
+   render() {
+      return(
+         
+      )
+   }
+}
+
+export default SmsContainer
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 import Aux from '../Auxiliary/Auxiliary'
 import './Layout.sass'
+import SmsContainer from './../../containers/SmsContainer'
 
 const layout = (props) => (
    <Aux>
-   
+      <SmsContainer />
    </Aux>
 )
 
